3436453acd8a9fc8328f61aae9cc74eaf1ef0680
Sang Yum <sangyum@gmail.com>
2019-12-02 17:55:41 -0800

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+const OFF = 0, WARN = 1, ERROR = 2;
+
+module.exports = exports = {
+    "extends": "airbnb/base",
+    "env": {
+        "jest": true
+    },
+    "rules": {
+        // Indent with 4 spaces
+        "indent": ["error", 4],
+        // "jest/no-disabled-tests": "warn",
+        // "jest/no-focused-tests": "error",
+        // "jest/no-identical-title": "error",
+        // "jest/prefer-to-have-length": "warn",
+        // "jest/valid-expect": "error"
+    }
+}
