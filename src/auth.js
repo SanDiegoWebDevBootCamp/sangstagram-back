158028f92cfdffd23110fffa402eca534cfd3723
Justin Yum <justinyum98@gmail.com>
2019-12-13 12:26:41 -0800

    basic authentication works

--- src/auth.js
+++ src/auth.js
@@ -2,11 +2,28 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 const User = {
     username: 'justinyum98',
     password: 'fakepassword',
 };
+
+passport.use(new GoogleStrategy(
+    {
+        clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+        callbackURL: 'http://localhost:5000/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log('profile:', profile);
+        return done(null, User);
+    },
+));
+
+passport.serializeUser((user, done) => done(null, user));
+
+passport.deserializeUser((user, done) => done(null, user));
 
 module.exports = {
     passport,
---
+++ src/database/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    // we're connected
+    console.log('I am connected');
+});
+
+module.exports = db;
